4524c64541dbe6863348fd92a9b6f7bc7683a966
Azael Monreal <A343427@alumnos.uaslp.mx>
2022-03-24 09:15:37 -0600

    Reto 3 - Apply formatting

--- Retos/R3.c
+++ Retos/R3.c
@@ -1,10 +1,12 @@
 /*
  * Encontrar la letra mas pequeña mayor que la que la de entrada
  *
- * Escriba un programa que dado un arreglo de caracteres letras que están ordenadas en orden ascedente y un caracter objetivo,
- * que te regrese el caracter mas pequeño en el arreglo que sea mayor que el caracter objetivo.
+ * Escriba un programa que dado un arreglo de caracteres letras que están
+ * ordenadas en orden ascedente y un caracter objetivo, que te regrese el
+ * caracter mas pequeño en el arreglo que sea mayor que el caracter objetivo.
  *
- * Considere a las letras circulares. Es decir si objetivo = 'z' y letras = ['a', 'b'], la respuesta deberá ser 'a'.
+ * Considere a las letras circulares. Es decir si objetivo = 'z' y letras =
+ * ['a', 'b'], la respuesta deberá ser 'a'.
  *
  * Ejemplo 1:
  * 	Entrada:  letras = ['c', 'f', 'j'], objetivo = 'a'
@@ -19,7 +21,8 @@
  * 	Salida:  'f'
  *
  * Consideraciones:
- * 	+ El arreglo de letras solo contendrá letras minúsculas del alfabeto en inglés.
+ * 	+ El arreglo de letras solo contendrá letras minúsculas del alfabeto en
+ * inglés.
  * 	+ El arreglo letras estará ordenado en orden ascendente.
  * 	+ El arreglo contiene al menos dos caracteres diferentes.
  * 	+ El caracter objetivo es una letra minúscula del alfabeto en inglés
@@ -38,28 +41,29 @@ int main()
 	printf("Target char: ");
 	scanf("%c", &c);
 
-	printf("Smallest bigger char in the array: %c\n", SmallBigLetter(in, SIZE, c));
+	printf("Smallest bigger char in the array: %c\n",
+				 SmallBigLetter(in, SIZE, c));
 }
 
 char SmallBigLetter(char letters[], int size, char target)
 {
-	int min = 0, max = size-1;
+	int min = 0, max = size - 1;
 
-	while(min <= max)
+	while (min <= max)
 	{
-		int guess = (min+max)/2;
+		int guess = (min + max) / 2;
 
-		if(letters[guess] == target)
-			return letters[(guess+1)%size];
+		if (letters[guess] == target)
+			return letters[(guess + 1) % size];
 
 		else
 		{
-			if(letters[guess] < target)
+			if (letters[guess] < target)
 				min = ++guess;
 			else
 				max = --guess;
 		}
 	}
 
-	return letters[min%size];
+	return letters[min % size];
 }
